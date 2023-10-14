8c04a96438c97b8317e3105e5838d943d73fa1da
Fabiola Alves <fabiola.ma.santos@gmail.com>
2023-10-14 16:25:06 -0300

    Add files via upload

---
+++ 2023-10-14/exercicios.js
@@ -0,0 +1,20 @@
+// ATIVIDADE DE FIXAÇÃO
+
+// 1. Declare uma variável contendo uma string
+let nome = "sábado"
+// 2. Declare uma variável contendo um número dentro de uma string
+let cpf = "04246342360"
+// 3. Declare uma variável com a sua idade
+let idade = 33
+// 4. Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+let primeiroNome = "Fabiola"
+let segundoNome = "Santos"
+let nomeCompleto = primeiroNome + " " + segundoNome
+alert(nomeCompleto)
+// 5. Coloque a seguinte frase em uma variável: E o pix, nada ainda?
+let frase = "E o pix, nada ainda?"
+// 6. Verifique o tipo da variável que contém o seu nome
+alert(typeof idade)
+// o typeof é útil para saber qual o tipo de variável está sendo usada.
+
+// 
---
+++ 2023-10-14/operadores.js
@@ -0,0 +1,50 @@
+// // operadores
+
+// // soma [lembrando que soma de uma string serve para concatenar]
+// let soma = 25 + 25
+// console.log(soma)
+
+// //subtracao
+// let subtracao = 100 - 50
+// console.log(subtracao)
+// // para ver o resultado abrir o arquivo js com o open live server, inspecionar e entrar em console
+// // configurações e ativar o preserve log (preservar registro)
+
+// console.log(+idade + 1)
+// // ao adicionar o + no começo da variavel, ele transforma a string quando tiver so numero
+// // em numero e adiciona mais 1
+
+// let idade = prompt("Qual a sua idade?")
+// alert (+idade +1)
+
+// let idade = prompt("Qual a sua idade?")
+// alert (idade +10)
+
+// Qual o resultado da seguinte expressão?
+// var total = 40 + 5 * 2 / 2 + 10; 55
+
+// Crie uma expressão que retorne NaN
+let nome = "22 Fabiola"
+alert(+nome)
+
+// Somar a string '10' com o número 10 e retornar 1010
+alert("10"+10)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+let n = 5
+alert(n + 1) // ou alert(++n) e decremento alert(--n)
+// let incremento = 49;
+// console.log(incremento++) //49
+// console.log(incremento) //50
+//
+//let incremento2 = 49;
+// console.log(++incremento2); //50
+// console.log(incremento2) //50
+
+// Como dividir o peso por 2?
+let numero = '90';
+let unidade = 'kg';
+let peso = numero + unidade; // '90kg'
+let pesoPorDois = peso / 2; // NaN (Not a Number)
+
+alert(numero / 2 + unidade)
